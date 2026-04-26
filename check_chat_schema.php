2d6273e829bc50ed365d7307b825c141f31560ba
PrimeSoofDev <chizinduudum@gmail.com>
2026-04-26 19:33:27 +0100

    Remove test script

--- check_chat_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "config/config.php";
-require_once "app/Core/Database.php";
-$db = Database::getInstance()->getConnection();
-$stmt = $db->query("DESCRIBE messages");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-$stmt2 = $db->query("DESCRIBE attachments");
-print_r($stmt2->fetchAll(PDO::FETCH_ASSOC));
